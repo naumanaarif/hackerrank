40a533190684a44591d93e55839f5aa3355e73cd
Nauman Arif <95040793+naumanaarif@users.noreply.github.com>
2022-06-07 21:53:53 +0500

    Added `jumpingOnClouds()`

---
+++ warmup_challenges/jumping_on_the_clouds.py
@@ -0,0 +1,23 @@
+
+
+def jumpingOnClouds(c):
+    i, jumps = 0, 0
+    n = len(c)
+
+    while i < n - 1:
+        # Jump two clouds, if possible
+        if i+2 < n and c[i+2] == 0:
+            jumps += 1
+            i += 2
+        # else, Jump one cloud
+        elif i+1 < n and c[i+1] == 0:
+            jumps += 1
+            i += 1
+
+    return jumps
+
+
+if __name__ == '__main__':
+    c = [0, 0, 1, 0, 0, 1, 0]
+    result = jumpingOnClouds(c)
+    print(result)
